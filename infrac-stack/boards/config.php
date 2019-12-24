51f39dd53911db70665cc4642cf3c862db73a53b
Craig Thacker <craigthackerx@gmail.com>
2019-12-24 02:06:17 +0000

    Switched wekan to Kanboard

---
+++ infrac-stack/boards/config.php
@@ -0,0 +1,14 @@
+// Database driver: sqlite, mysql or postgres (sqlite by default)
+define('DB_DRIVER', 'mysql');
+
+// Mysql/Postgres username
+define('DB_USERNAME', 'kanboard');
+
+// Mysql/Postgres password
+define('DB_PASSWORD', 'mypassword');
+
+// Mysql/Postgres hostname
+define('DB_HOSTNAME', 'localhost');
+
+// Mysql/Postgres database name
+define('DB_NAME', 'kanboard');
